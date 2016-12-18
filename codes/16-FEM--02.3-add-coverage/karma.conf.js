f547ef6875b93415761e4a0b2d9caa269f12c0ea
Apollo Tang <tang.apollo@gmail.com>
2016-12-18 15:39:30 -0500

    ....

---
+++ codes/16-FEM--02.3-add-coverage/karma.conf.js
@@ -0,0 +1,33 @@
+process.env.BABEL_ENV = 'test'
+const webpackEnv = {test: true}
+const webpackConfig = require('./webpack.config.babel')(webpackEnv)
+
+const fileGlob = 'src/**/*.test.js'
+
+module.exports = config => {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [fileGlob],
+    preprocessors: {
+      [fileGlob]: ['webpack']
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {noInfo: true},
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      reporters: [
+        {type: 'lcov', dir: 'coverage/', subdir: '.'},
+        {type: 'json', dir: 'coverage/', subdir: '.'},
+        {type: 'text-summary'},
+      ],
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    singleRun: true,
+    concurrency: Infinity,
+  })
+}
